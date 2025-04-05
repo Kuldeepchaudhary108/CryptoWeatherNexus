c3564e4a8f915f537742748284db9b6514c16489
kuldeepchaudhary75 <choudharykuldeep5511@gmail.com>
2025-04-05 21:56:52 +0530

    adding frontend of weather page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,34 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import WeatherPage from "./components/weather/Weather.jsx";
+import Crypto from "./components/crypto/index.jsx";
 import App from "./App.jsx";
+
+function Main() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<App />} />
+          <Route path="/weather" element={<WeatherPage />} />
+          <Route path="/crypto" element={<Crypto />} />
+        </Route>
+      </>
+    )
+  );
+  return <RouterProvider router={router} />;
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Main />
   </StrictMode>
 );
